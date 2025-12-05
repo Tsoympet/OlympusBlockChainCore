85a1f701a85f8833d22361d95157faed598c82b6
Tsoympet <tsoympet7@gmail.com>
2025-12-05 09:25:31 +0200

    Create contract_loader.cpp

---
+++ src/l2/pvm/contract_loader.cpp
@@ -0,0 +1,10 @@
+#include "contract_loader.h"
+
+namespace parthenon {
+
+std::vector<uint8_t> ContractLoader::load(const std::string& path) {
+    // TODO: read bytecode from file
+    return {0xFF}; // STOP
+}
+
+}
