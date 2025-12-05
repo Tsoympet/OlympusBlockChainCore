141302299180bda592ceaa890c7676228deb4200
Tsoympet <tsoympet7@gmail.com>
2025-12-05 08:45:55 +0200

    Create sha3.cpp

---
+++ src/core/crypto/sha3.cpp
@@ -0,0 +1,10 @@
+#include "sha3.h"
+
+namespace parthenon {
+
+std::string SHA3::keccak256(const std::string& input) {
+    // TODO: real Keccak implementation
+    return "KECCAK256_PLACEHOLDER";
+}
+
+}
