29c15b5835f01e24f04b989ef04f796a6ac25d06
Tsoympet <tsoympet7@gmail.com>
2025-12-05 08:48:42 +0200

    Create ripemd160.cpp

---
+++ src/core/crypto/ripemd160.cpp
@@ -0,0 +1,10 @@
+#include "ripemd160.h"
+
+namespace parthenon {
+
+std::string RIPEMD160::hash(const std::string& data) {
+    // TODO: implement RIPEMD160
+    return "RIPEMD160_PLACEHOLDER";
+}
+
+}
