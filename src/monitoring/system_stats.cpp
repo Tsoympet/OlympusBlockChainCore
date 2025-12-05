cd5ee266f2428917e3f1eb00bf050f2c825476df
Tsoympet <tsoympet7@gmail.com>
2025-12-05 11:23:35 +0200

    Create system_stats.cpp

---
+++ src/monitoring/system_stats.cpp
@@ -0,0 +1,25 @@
+#include "system_stats.h"
+
+namespace parthenon {
+
+double SystemStats::getCpuUsage() {
+    // TODO
+    return 0.0;
+}
+
+double SystemStats::getMemoryUsage() {
+    // TODO
+    return 0.0;
+}
+
+uint64_t SystemStats::getDiskUsage() {
+    // TODO
+    return 0;
+}
+
+double SystemStats::getNetworkLatency() {
+    // TODO
+    return 0.0;
+}
+
+}
