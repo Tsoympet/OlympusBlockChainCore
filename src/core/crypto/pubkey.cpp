ffddbb223120428d325a41ae8554e342cea2e96a
Tsoympet <tsoympet7@gmail.com>
2025-12-05 08:56:44 +0200

    Create pubkey.cpp

---
+++ src/core/crypto/pubkey.cpp
@@ -0,0 +1,15 @@
+#include "pubkey.h"
+
+namespace parthenon {
+
+std::string PubKey::getCompressed() const {
+    // TODO
+    return "COMPRESSED_PUBKEY";
+}
+
+std::string PubKey::getUncompressed() const {
+    // TODO
+    return "UNCOMPRESSED_PUBKEY";
+}
+
+}
