fd66ce8e7c2cba8768ab7d60abc464c775fbe4b7
Tsoympet <tsoympet7@gmail.com>
2025-12-05 08:59:47 +0200

    Create base58.cpp

---
+++ src/core/crypto/base58.cpp
@@ -0,0 +1,15 @@
+#include "base58.h"
+
+namespace parthenon {
+
+std::string Base58::encode(const std::string& input) {
+    // TODO
+    return "BASE58_PLACEHOLDER";
+}
+
+std::string Base58::decode(const std::string& input) {
+    // TODO
+    return "DECODE_PLACEHOLDER";
+}
+
+}
