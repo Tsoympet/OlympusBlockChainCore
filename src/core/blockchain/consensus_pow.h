bf7ce79c0f4f1c917e4118ffa2b8cde7280ab9d1
Tsoympet <tsoympet7@gmail.com>
2025-12-06 15:58:38 +0200

    Update consensus_pow.h

--- src/core/blockchain/consensus_pow.h
+++ src/core/blockchain/consensus_pow.h
@@ -1,1 +1,16 @@
-// placeholder
+#ifndef PARTHENON_CONSENSUS_POW_H
+#define PARTHENON_CONSENSUS_POW_H
+
+#include "block.h"
+
+namespace parthenon {
+
+class PoWConsensus {
+public:
+    static bool validateBlock(const Block& block);
+    static bool meetsDifficulty(const Block& block);
+};
+
+}
+
+#endif
