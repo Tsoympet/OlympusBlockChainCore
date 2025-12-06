49fbf5a6f080c995f731ff35872fb6f9c0eaefa0
Tsoympet <tsoympet7@gmail.com>
2025-12-06 15:57:28 +0200

    Update difficulty.cpp

--- src/core/blockchain/difficulty.cpp
+++ src/core/blockchain/difficulty.cpp
@@ -1,1 +1,10 @@
-// placeholder
+#include "difficulty.h"
+
+namespace parthenon {
+
+uint32_t Difficulty::calculateNext(uint32_t prevDifficulty, uint64_t timeDelta) {
+    // TODO: implement real adjustment algorithm
+    return prevDifficulty;
+}
+
+}
