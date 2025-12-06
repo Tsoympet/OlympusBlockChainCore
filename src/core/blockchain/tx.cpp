9a33190756622a210cfa8b59ee9ad3fe5873d2b3
Tsoympet <tsoympet7@gmail.com>
2025-12-06 15:55:00 +0200

    Update tx.cpp

--- src/core/blockchain/tx.cpp
+++ src/core/blockchain/tx.cpp
@@ -1,1 +1,17 @@
-// placeholder
+#include "tx.h"
+#include "crypto/sha2.h"
+
+namespace parthenon {
+
+std::string Transaction::txid() const {
+    // TODO: proper serialization before hashing
+    return SHA256::hash(std::to_string(version) + std::to_string(lockTime));
+}
+
+bool Transaction::validateBasic() const {
+    if (inputs.empty() || outputs.empty()) return false;
+    return true;
+}
+
+}
+
