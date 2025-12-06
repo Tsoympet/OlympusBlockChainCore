e8b02bbd2f3694a22d1dc79794bf35314a922e5b
Tsoympet <tsoympet7@gmail.com>
2025-12-06 15:56:46 +0200

    Update mempool.cpp

--- src/core/blockchain/mempool.cpp
+++ src/core/blockchain/mempool.cpp
@@ -1,1 +1,19 @@
-// placeholder
+#include "mempool.h"
+
+namespace parthenon {
+
+bool Mempool::addTransaction(const Transaction& tx) {
+    pool[tx.txid()] = tx;
+    return true;
+}
+
+bool Mempool::removeTransaction(const std::string& txid) {
+    return pool.erase(txid) > 0;
+}
+
+bool Mempool::exists(const std::string& txid) const {
+    return pool.find(txid) != pool.end();
+}
+
+}
+
