2b3f166e0141d9e93f3a5991c15fc313de5ee1dc
Tsoympet <tsoympet7@gmail.com>
2025-12-06 15:55:53 +0200

    Update chain.h

--- src/core/blockchain/chain.h
+++ src/core/blockchain/chain.h
@@ -1,1 +1,26 @@
-// placeholder
+#ifndef PARTHENON_CHAIN_H
+#define PARTHENON_CHAIN_H
+
+#include "block.h"
+#include "utxo.h"
+#include <vector>
+
+namespace parthenon {
+
+class Blockchain {
+public:
+    std::vector<Block> chain;
+    UTXOSet utxoSet;
+
+    Blockchain();
+
+    const Block& getLatestBlock() const;
+    bool addBlock(const Block& block);
+
+    bool validateBlock(const Block& block) const;
+    bool connectBlock(const Block& block);
+};
+
+}
+
+#endif
