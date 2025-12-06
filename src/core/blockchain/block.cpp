8f6577919014e4fa6b3d4f362a3d9621c1840a50
Tsoympet <tsoympet7@gmail.com>
2025-12-06 15:54:23 +0200

    Update block.cpp

--- src/core/blockchain/block.cpp
+++ src/core/blockchain/block.cpp
@@ -1,1 +1,26 @@
-// placeholder
+#include "block.h"
+#include "crypto/sha2.h"
+
+namespace parthenon {
+
+std::string BlockHeader::hash() const {
+    return SHA256::hash(prevHash + merkleRoot + std::to_string(timestamp) +
+                        std::to_string(difficultyTarget) + std::to_string(nonce));
+}
+
+std::string Block::getHash() const {
+    return header.hash();
+}
+
+std::string Block::calculateMerkleRoot() const {
+    // TODO: implement Merkle root calculation
+    return "MERKLE_PLACEHOLDER";
+}
+
+bool Block::validateBasic() const {
+    // TODO: implement sanity checks
+    return true;
+}
+
+}
+
