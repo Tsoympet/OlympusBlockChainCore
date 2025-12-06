c0f9ca2c515c8ca4faad4f476a946a0e6db8e9b8
Tsoympet <tsoympet7@gmail.com>
2025-12-06 15:56:11 +0200

    Update chain.cpp

--- src/core/blockchain/chain.cpp
+++ src/core/blockchain/chain.cpp
@@ -1,1 +1,29 @@
-// placeholder
+#include "chain.h"
+
+namespace parthenon {
+
+Blockchain::Blockchain() {
+    // TODO: Genesis block creation
+}
+
+const Block& Blockchain::getLatestBlock() const {
+    return chain.back();
+}
+
+bool Blockchain::addBlock(const Block& block) {
+    if (!validateBlock(block)) return false;
+    chain.push_back(block);
+    return true;
+}
+
+bool Blockchain::validateBlock(const Block& block) const {
+    // TODO: validate PoW/PoS + merkle + timestamp rules
+    return true;
+}
+
+bool Blockchain::connectBlock(const Block& block) {
+    return addBlock(block);
+}
+
+}
+
