85cc6335a255f70fb8e668645c12c82f76173cc6
Tsoympet <tsoympet7@gmail.com>
2025-12-06 15:56:29 +0200

    Update mempool.h

--- src/core/blockchain/mempool.h
+++ src/core/blockchain/mempool.h
@@ -1,1 +1,21 @@
-// placeholder
+#ifndef PARTHENON_MEMPOOL_H
+#define PARTHENON_MEMPOOL_H
+
+#include "tx.h"
+#include <unordered_map>
+
+namespace parthenon {
+
+class Mempool {
+public:
+    std::unordered_map<std::string, Transaction> pool;
+
+    bool addTransaction(const Transaction& tx);
+    bool removeTransaction(const std::string& txid);
+    bool exists(const std::string& txid) const;
+};
+
+}
+
+#endif
+
