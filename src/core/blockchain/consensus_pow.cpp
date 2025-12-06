69a57d03c16900edc5c545bfddf49bebc4d00c5c
Tsoympet <tsoympet7@gmail.com>
2025-12-06 15:58:13 +0200

    Update consensus_pow.cpp

--- src/core/blockchain/consensus_pow.cpp
+++ src/core/blockchain/consensus_pow.cpp
@@ -1,1 +1,15 @@
-// placeholder
+#include "consensus_pow.h"
+#include "crypto/sha2.h"
+
+namespace parthenon {
+
+bool PoWConsensus::validateBlock(const Block& block) {
+    return meetsDifficulty(block);
+}
+
+bool PoWConsensus::meetsDifficulty(const Block& block) {
+    // TODO: check block.header.hash() against difficulty target
+    return true;
+}
+
+}
