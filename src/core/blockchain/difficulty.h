0b67bbcb848e6db9302036fda79f44627fe5aff0
Tsoympet <tsoympet7@gmail.com>
2025-12-06 15:57:07 +0200

    Update difficulty.h

--- src/core/blockchain/difficulty.h
+++ src/core/blockchain/difficulty.h
@@ -1,1 +1,16 @@
-// placeholder
+#ifndef PARTHENON_DIFFICULTY_H
+#define PARTHENON_DIFFICULTY_H
+
+#include <cstdint>
+
+namespace parthenon {
+
+class Difficulty {
+public:
+    static uint32_t calculateNext(uint32_t prevDifficulty, uint64_t timeDelta);
+};
+
+}
+
+#endif
+
