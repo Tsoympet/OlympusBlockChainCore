be95e5229d482bd55135c4561556e78af3a22843
Tsoympet <tsoympet7@gmail.com>
2025-12-06 15:54:05 +0200

    Update block.h

--- src/core/blockchain/block.h
+++ src/core/blockchain/block.h
@@ -1,1 +1,36 @@
-// placeholder
+#ifndef PARTHENON_BLOCK_H
+#define PARTHENON_BLOCK_H
+
+#include <vector>
+#include <cstdint>
+#include <string>
+#include "tx.h"
+
+namespace parthenon {
+
+class BlockHeader {
+public:
+    uint32_t version;
+    std::string prevHash;
+    std::string merkleRoot;
+    uint64_t timestamp;
+    uint32_t difficultyTarget;
+    uint32_t nonce;
+
+    std::string hash() const;
+};
+
+class Block {
+public:
+    BlockHeader header;
+    std::vector<Transaction> transactions;
+
+    std::string getHash() const;
+    std::string calculateMerkleRoot() const;
+
+    bool validateBasic() const;
+};
+
+}
+
+#endif
