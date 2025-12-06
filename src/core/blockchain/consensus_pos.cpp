e13a9e12618f03bbbb68639a2debac63111c74eb
Tsoympet <tsoympet7@gmail.com>
2025-12-06 15:59:14 +0200

    Update consensus_pos.cpp

--- src/core/blockchain/consensus_pos.cpp
+++ src/core/blockchain/consensus_pos.cpp
@@ -1,1 +1,11 @@
-// placeholder
+#include "consensus_pos.h"
+
+namespace parthenon {
+
+bool PoSConsensus::validateBlock(const Block& block) {
+    // TODO: check staking rules
+    return true;
+}
+
+}
+
